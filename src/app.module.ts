f3176384cb8b242d23969e990737b88e1e8b8cb4
rehan <rehan.abbas@planlabsolutions.com>
2025-04-13 12:00:00 +0500

    Backdated commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { TelegramService } from './telegram/telegram.service';
       synchronize:true, 
       entities:[User]
     }),
-    TelegramModule,
+    TelegramModule, 
   ],
   controllers: [],   
   providers: [],
